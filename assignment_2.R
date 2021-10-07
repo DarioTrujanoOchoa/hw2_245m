ca19b48b44c9f29024b9317c66d02d60b6432454
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2021-10-06 22:31:59 -0700

    Part 1.1 finished

--- assignment_2.R
+++ assignment_2.R
@@ -1,1 +1,20 @@
-#  assignment_2.R
+# assignment_2.R
+# Dario Trujano Ochoa
+
+library(tidyverse)
+
+# Part 1 ----
+## 1 ----
+### a
+airbnb <- read_csv("assign_2.csv")
+airbnb <- airbnb %>% tibble()
+
+### b
+View(airbnb)
+colnames(airbnb)
+
+### c
+
+airbnb <-airbnb %>% 
+  rename(neighborhood=neighbourhood)
+
