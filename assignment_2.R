ec0b85ef054865f74c186c74ebbfed8816dd5626
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2021-10-12 21:04:49 -0700

    Finish part 2

--- assignment_2.R
+++ assignment_2.R
@@ -19,4 +19,27 @@ airbnb <-airbnb %>%
   rename(neighborhood=neighbourhood)
 
 # Part 2 ----
+## a
 neighborhoods <- airbnb %>% count(neighborhood)
+
+## b
+neighborhoods <- neighborhoods %>% 
+  filter(!is.na(neighborhood)) %>% 
+  arrange(-n) %>% 
+  head(20)
+
+## c
+airbnb_top_neighborhoods <-
+airbnb %>% filter(neighborhood %in% neighborhoods$neighborhood)
+
+## d
+summary_stats_top_neighborhoods <-
+airbnb_top_neighborhoods %>% 
+  group_by(neighborhood) %>% 
+  summarise(avg_square_feet = mean(square_feet,na.rm=T), 
+            avg_price = mean(price,na.rm=T), 
+            sd_price = sd(price,na.rm=T), 
+            max_price = max(price), 
+            min_price = min(price)) %>% 
+  arrange(-avg_square_feet)
+
